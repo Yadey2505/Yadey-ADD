d55b17f0e3a9f093219c668c4dbcd9ad78f4aafa
Yadey2505 <yadeyrs05@gmail.com>
2025-09-19 13:05:18 +0100

    Nuevo

--- yadeybash.sh
+++ yadeybash.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-#########################################
-#	Menu Yadey Rivero		#
-#########################################
-
+echo "#########################################"
+echo "#	    Menu Yadey Rivero           #"
+echo "#########################################"
+echo ""
 #Opciones del menú
 menu(){
    echo "***********************"
    echo "1.Bisiesto"
    echo "2. Red"
+   echo "3."
+   echo "4."
+   echo "5."
+   echo "6."
+   echo "7."
+   echo "8."
+   echo "9."
+   echo "10."
+   echo "11."
+   echo "12."
+   echo "13."
+   echo "14."
+   echo "15."
    echo "***********************"
 
 #Interaccion con el usuario
@@ -25,26 +38,24 @@ menu(){
          fi
          ;;
 
-     2)
-archivo="/etc/netplan/01-network-manager-all.yaml"
-	read -p "Introduce la interfaz de red (ej: enp0s3, eth0): " interfaz
-	read -p "Introduce una IP: " ip
-	read -p "Introduce una mascara (ej: 24 ): " masca
-	read -p "Introduce una puerta de enlace: " gateway
-	read -p "Introduce un DNS: " dns
+      2)
+	 read -p "Introduce una IP: " ip
+	 read -p "Introduce una mascara (ej: 24 ): " masca
+	 read -p "Introduce una puerta de enlace: " gateway
+	 read -p "Introduce un DNS: " dns
 
-	cat /etc/netplan/redconfig > /etc/netplan/01-network-manager-all.yaml
-	echo "     - $ip/masca " >> /etc/netplan/01-network-manager-all.yaml
-	echo "    routes:" >> /etc/netplan/01-network-manager-all.yaml
-	echo "     - to: default" >> /etc/netplan/01-network-manager-all.yaml
-	echo "       via: $gate" >> /etc/netplan/01-network-manager-all.yaml
-	echo "    nameservers:" >> /etc/netplan/01-network-manager-all.yaml
-	echo "      addresses: [$dns]" >> /etc/netplan/01-network-manager-all.yaml
-	netplan apply > /dev/null
-	sleep 5
-	ip a
+         echo "      - $ip/$masca" >> /etc/netplan/01-network-manager-all.yaml
+         echo "    routes:" >> /etc/netplan/01-network-manager-all.yaml
+         echo "      - to: default" >> /etc/netplan/01-network-manager-all.yaml
+         echo "        via: $gateway" >> /etc/netplan/01-network-manager-all.yaml
+         echo "    nameservers:" >> /etc/netplan/01-network-manager-all.yaml
+         echo "      addresses: [$dns]" >> /etc/netplan/01-network-manager-all.yaml
+	 netplan apply > /dev/null
+	 sleep 5
+	 ip a
 
-      ;;
-   esac
+       ;;
+
+    esac
 }
 menu
